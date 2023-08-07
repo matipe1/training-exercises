68d3cbd3882e5cec0c5e687cb00c93f711167090
Diego <dmpetitto2004@gmail.com>
2023-08-06 23:36:55 -0300

    Improve exercise

--- Python/strings/alphabet_rangoli.py
+++ Python/strings/alphabet_rangoli.py
@@ -1,42 +1,33 @@
 def print_rangoli(size):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    n = size
-    letters = [alphabet[i] for i in range(int(n))]
+    letters = alphabet[:size]
+    complete = [letters[n] for n in range(size)]
+    complete.reverse()
+    complete.extend([letters[n] for n in range(1, size)])  # input(5): ['e', 'd', 'c', 'b', 'a', 'b', 'c', 'd', 'e']
+    middle = ((size * 2) // 2) - 1
 
-    letters.reverse()
-    complete = []
+    for i in range(size * 2 - 1):
 
-    for i in range(len(letters)):
-        middle = letters[i]
-        first = [letters[j] for j in range(i)]
-        second = [letters[k] for k in range(i)]
-        second.reverse()
+        if i < middle:
+            left = complete[:i]
+            right = complete[i::-1]
+            middle_inside = [complete[i]]
 
-        complete.extend(first)
-        complete.extend(middle)
-        complete.extend(second)
+            print(('-'.join(left + middle_inside + right[1:])).center(len(complete) * 2 - 1, '-'))
 
-        print(('-'.join(complete)).center((((int(n) - 1) * 4) + 1), '-'))  # How it looks in each line
+    print('-'.join(complete))
 
-        if not i == len(letters) - 1:
-            complete = []  # Delete
+    for i in reversed(range(size * 2 - 1)):
 
-    complete_2 = complete
+        if i < middle:
+            left = complete[:i]
+            right = complete[i::-1]
+            middle_inside = [complete[i]]
 
-    complete_2.reverse()
-    letters.reverse()
-
-    for j in range(1, len(letters)):
-        middle = letters[j]
-        # print(('-'.join(complete_2)).center((((int(n) - 1) * 4) + 1), '-'))
-        left = complete[0:len(letters) - 1 - j]
-        # print('left', left)
-        right = complete_2[0: len(letters) - 1 - j]
-        right.reverse()
-        # print('right', right)
-        print(('-'.join(left)).rjust(((int(n) - 1) * 2) - 1, '-') + middle.center(3, '-') + ('-'.join(right)).ljust(((int(n) - 1) * 2) - 1, '-'))
+            print(('-'.join(left + middle_inside + right[1:])).center(len(complete) * 2 - 1, '-'))
 
 
 if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)
+
